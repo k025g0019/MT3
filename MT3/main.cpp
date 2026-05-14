c6b8cc219642091d1e5a53b909b08c0a17305e06
LC1B_29_mano <k025g0019@g.neec.ac.jp>
2026-05-14 22:09:47 +0900

    01_00

--- MT3/main.cpp
+++ MT3/main.cpp
@@ -13,7 +13,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	Vector3 scale{1.2f, 0.79f, -2.1f};
 	Vector3 rotate{0.4f, 1.43f, -0.8f};
 
-	Vector3 translate{2.7f, -4.15f, 1.57f};
 
 	while (Novice::ProcessMessage() == 0) {
 		Novice::BeginFrame();
