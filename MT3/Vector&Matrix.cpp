acc7f55c0664c1d9b6b7f731737fa128f9ec2067
LC1B_29_mano <k025g0019@g.neec.ac.jp>
2026-05-15 12:47:19 +0900

    初期化

--- MT3/Vector&Matrix.cpp
+++ MT3/Vector&Matrix.cpp
@@ -21,7 +21,7 @@ Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix) {
 
 
 Matrix4x4 MakeTranslationMatrix(const Vector3& translation) {
-	Matrix4x4 MakeTransla;
+	Matrix4x4 MakeTransla = {0};
 	MakeTransla.matrix[0][0] = 1.0f;
 	MakeTransla.matrix[0][1] = 0.0f;
 	MakeTransla.matrix[0][2] = 0.0f;
@@ -46,7 +46,7 @@ Matrix4x4 MakeTranslationMatrix(const Vector3& translation) {
 }
 
 Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
-	Matrix4x4 MakeScale;
+	Matrix4x4 MakeScale = {0};
 	MakeScale.matrix[0][0] = scale.x;
 	MakeScale.matrix[0][1] = 0.0f;
 	MakeScale.matrix[0][2] = 0.0f;
