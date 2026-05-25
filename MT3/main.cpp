b7c6f3b9dfc842066f7aac354ddd3aaa2ae08779
LC1B_29_mano <k025g0019@g.neec.ac.jp>
2026-05-25 14:45:02 +0900

    正規化

--- MT3/main.cpp
+++ MT3/main.cpp
@@ -36,6 +36,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		Matrix4x4 viewportMatrix = MakeViewportMatrix(
 			0.0f, 0.0f, static_cast<float>(kWindowWidth), static_cast<float>(kWindowHeight), 0.0f, 1.0f);
 
+
 		DrawGrid(viewProjectionMatrix, viewportMatrix);
 
 
