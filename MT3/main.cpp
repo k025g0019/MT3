77c3e977f254a81cce5cd5068e700fadbb82c0a5
LC1B_29_mano <k025g0019@g.neec.ac.jp>
2026-05-29 11:48:52 +0900

    komitt

--- MT3/main.cpp
+++ MT3/main.cpp
@@ -23,18 +23,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	OrbitCamera orbitCamera{};
 	InitializeOrbitCamera(orbitCamera, {0.0f, 0.0f, 0.0f}, {0.0f, 1.9f, -6.49f});
 
-	Vector3 a{ 0.2f,1.0f,0.0f };
-	Vector3 b{ 2.4f,3.1f,1.2f };
-
-	Vector3 c = a + b;
-	Vector3 d = a - b;
-	Vector3 e = a * 2.4f;
-	Vector3 rotate{ 0.4f,1.43f,-0.8f };
-	Matrix4x4 rotatexMatrix = MakeRotateXMatrix(rotate.x);
-	Matrix4x4 rotateyMatrix = MakeRotateYMatrix(rotate.y);
-	Matrix4x4 rotatezMatrix = MakeRotateZMatrix(rotate.z);
-
-	Matrix4x4 rotateMatrix =rotatexMatrix*rotateyMatrix*rotatezMatrix;
 	while (Novice::ProcessMessage() == 0) {
 		Novice::BeginFrame();
 
@@ -71,13 +59,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 #ifdef USE_IMGUI
 		ImGui::Begin("Debug Window");
 
-		ImGui::DragFloat3("a", &a.x, 0.01f);
-		ImGui::DragFloat3("b", &b.x, 0.01f);
-		ImGui::DragFloat3("rotate", &rotate.x, 0.01f);
-
-		c = a + b;
-		d = a - b;
-		e = a * 2.4f;
+		ImGui::End();
 
 
 #endif
