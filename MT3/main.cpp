274318824752f85e1abf5787a912afe8d2b6360d
LC1B_29_mano <k025g0019@g.neec.ac.jp>
2026-05-29 11:49:02 +0900

    komitt

--- MT3/main.cpp
+++ MT3/main.cpp
@@ -55,7 +55,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		// ここにコードを追加していく
 		//=====================================================
 
-	
+
 #ifdef USE_IMGUI
 		ImGui::Begin("Debug Window");
 
